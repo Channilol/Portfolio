c4c72ee531e81a9aa7cb1eddb32f1f4beb95bdb9
Your Name <you@example.com>
2024-08-23 16:54:39 +0200

    boh

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 //? FUNZIONE PER IL MOUSE CUSTOM
 
 const cursorDot = document.querySelector(".cursor-dot");
-const cursorOutline = document.querySelector(".cursor-outline");
-const hoverObj = [];
 const titles = document.querySelectorAll(".hero-title");
+const slider = document.querySelector(".hero-slider");
 
 window.addEventListener("mousemove", function (e) {
   const posX = e.clientX;
@@ -12,29 +11,17 @@ window.addEventListener("mousemove", function (e) {
   cursorDot.style.left = `${posX}px`;
   cursorDot.style.top = `${posY}px`;
 
-  cursorOutline.animate(
-    {
-      left: `${posX}px`,
-      top: `${posY}px`,
-    },
-    { duration: 100, fill: "forwards", easing: "ease-in" }
-  );
-
   titles.forEach((element) => {
     element.addEventListener("mouseover", function () {
       cursorDot.style.width = "60px";
       cursorDot.style.height = "60px";
       cursorDot.style.transition = ".1s ease";
-
-      cursorOutline.style.opacity = "0";
     });
 
     element.addEventListener("mouseout", function () {
-      cursorDot.style.width = "15px";
-      cursorDot.style.height = "15px";
-      cursorDot.style.transition = "0s ease";
-
-      cursorOutline.style.opacity = "1";
+      cursorDot.style.width = "30px";
+      cursorDot.style.height = "30px";
+      cursorDot.style.transition = ".1s ease";
     });
   });
 });
